6c9948d6bbca07dff3e94410eb5fadd7a60e4566
chap95 <chap95@likelion.org>
2024-12-27 11:39:23 +0900

    chore: 주석 제거

--- panel.js
+++ panel.js
@@ -66,10 +66,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 const handleClick = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     if (tabs.length > 0) {
-      // Background Script로 메시지 전송
       chrome.runtime.sendMessage({
         action: "parseSchema",
-        tabId: tabs[0].id, // 현재 탭 ID 전달
+        tabId: tabs[0].id,
       });
     }
   });
